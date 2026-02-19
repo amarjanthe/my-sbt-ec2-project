7f6182b57ce752acd3b14483cb2cefab0f7dc711
amarjanthe <159176597+amarjanthe@users.noreply.github.com>
2026-02-19 17:27:06 +0530

    Refactor Hello object to use Http4s and IOApp

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,4 +1,27 @@
-object Hello extends App {
-  println("Hello from EC2 deployment!")
-  println("Amar is aws devops enginerr learning harness also")
+import cats.effect._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.ember.server._
+import org.http4s.implicits._
+import com.comcast.ip4s._
+
+object Hello extends IOApp {
+
+  val routes = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      Ok("Hello from EC2! Amar is AWS DevOps Engineer learning Harness!")
+
+    case GET -> Root / "health" =>
+      Ok("App is running!")
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"9000")
+      .withHttpApp(routes.orNotFound)
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
 }
